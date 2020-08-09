4caaa1f29a812cc40e02bde2a3ef7904704594a6
maiorem <61309793+maiorem@users.noreply.github.com>
2020-08-09 17:10:29 +0900

    blog rest 설정

---
+++ Blog-Server/src/main/java/com/blog/board/controller/BoardRestController.java
@@ -0,0 +1,15 @@
+package com.blog.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/boards")
+public class BoardRestController {
+	
+	
+	
+	
+	
+
+}
---
+++ Blog-Server/src/main/java/com/blog/board/dao/BoardDao.java
@@ -0,0 +1,5 @@
+package com.blog.board.dao;
+
+public interface BoardDao {
+
+}
