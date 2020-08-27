1c624715cfdedeca9ac211391fd8f1d7a4116625
maiorem <maiorem00@gmail.com>
2020-08-27 18:02:24 +0900

    jpa join

--- hello.jpa/src/main/java/hello/jpa/entity/Member.java
+++ hello.jpa/src/main/java/hello/jpa/entity/Member.java
@@ -6,9 +6,12 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -22,16 +25,33 @@ public class Member {
 	private String name;
 	private int age;
 	
-	@Temporal(TemporalType.TIMESTAMP) 
+	@Temporal(TemporalType.TIMESTAMP)
 	private Date regdate;
 	
 	@Enumerated(EnumType.STRING)
 	private MemberType memberType;
 	
+//	@Column(name="TEAM_ID")
+//	private Long teamId;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "TEAM_ID")
+	private Team team;
 	
 	
-	
-	
+	public Team getTeam() {
+		return team;
+	}
+	public void setTeam(Team team) {
+		this.team = team;
+	}
+	public Date getRegdate() {
+		return regdate;
+	}
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
 	public int getAge() {
 		return age;
 	}
@@ -57,6 +77,11 @@ public class Member {
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Member [id=" + id + ", name=" + name + ", age=" + age + ", regdate=" + regdate + ", memberType="
+				+ memberType + ", team=" + team + "]";
 	}
 
 	
