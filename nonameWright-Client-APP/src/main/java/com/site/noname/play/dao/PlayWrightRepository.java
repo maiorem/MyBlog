002601d3cb029ff5bca82f9d3a1f198f7653fe5b
maiorem <maiorem00@gmail.com>
2020-09-02 19:57:57 +0900

    폴더 생성

---
+++ nonameWright-Client-APP/src/main/java/com/site/noname/play/dao/PlayWrightRepository.java
@@ -0,0 +1,9 @@
+package com.site.noname.play.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.site.noname.play.model.PlayWright;
+
+public interface PlayWrightRepository extends JpaRepository<PlayWright, Integer> {
+
+}
---
+++ nonameWright-Client-APP/src/main/java/com/site/noname/play/model/PlayWright.java
@@ -0,0 +1,42 @@
+package com.site.noname.play.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Table(name="play")
+public class PlayWright {
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private int playId;
+	private String sort;
+	private String title;
+	private String theater;
+	private String posterImg;
+	private int year;
+	@JsonFormat(pattern = "yyyy.MM.dd")
+	private Date startDate;
+	@JsonFormat(pattern = "MM.dd")
+	private Date endDate;
+	private String role;
+	private int status;
+	
+
+}
