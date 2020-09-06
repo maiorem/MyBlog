9dbe4a69dad77dafc3dbe10e78ef369f8e83153e
maiorem <maiorem00@gmail.com>
2020-09-06 23:40:36 +0900

    아이디 삽입

--- nonameWright-Client-APP/src/main/java/com/site/noname/blarcomments/model/BoardComment.java
+++ nonameWright-Client-APP/src/main/java/com/site/noname/blarcomments/model/BoardComment.java
@@ -1,5 +1,13 @@
 package com.site.noname.blarcomments.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class BoardComment {
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long boardCommId;
+	
 
 }
--- nonameWright-Client-APP/src/main/java/com/site/noname/studycomments/model/StudyComment.java
+++ nonameWright-Client-APP/src/main/java/com/site/noname/studycomments/model/StudyComment.java
@@ -1,5 +1,8 @@
 package com.site.noname.studycomments.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
@@ -7,6 +10,9 @@ import com.site.noname.study.model.StudyEntity;
 
 public class StudyComment {
 	
+	
+	@Id @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long studyCommId;
 	@ManyToOne
 	@JoinColumn(name = "study_id")
 	private StudyEntity study;
