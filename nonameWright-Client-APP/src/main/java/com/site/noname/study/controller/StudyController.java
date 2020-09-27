aaed49f58cfc9b55b3666207659fef8d775426ef
maiorem <maiorem00@gmail.com>
2020-09-27 19:45:46 +0900

    study controller

---
+++ nonameWright-Client-APP/src/main/java/com/site/noname/study/controller/StudyController.java
@@ -0,0 +1,10 @@
+package com.site.noname.study.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/blog")
+public class StudyController {
+
+}
