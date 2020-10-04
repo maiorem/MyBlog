64b67e80314df934809df3609c7228be4217285d
maiorem <maiorem00@gmail.com>
2020-10-04 22:27:25 +0900

    포트폴리오 이미지 정리

--- nonameWright-Client-APP/src/main/java/com/site/noname/study/service/StudyWriteService.java
+++ nonameWright-Client-APP/src/main/java/com/site/noname/study/service/StudyWriteService.java
@@ -1,12 +1,8 @@
 package com.site.noname.study.service;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.site.noname.study.model.StudyWriteRequest;
 import com.site.noname.study.repository.StudyRepository;
 
 @Service
@@ -15,9 +11,6 @@ public class StudyWriteService {
 	@Autowired
 	private StudyRepository studyRepository;
 
-	@Transactional
-	public Long save(StudyWriteRequest writeRequest, HttpServletRequest request) {
-		return studyRepository.save(writeRequest.toEntity()).getStudyId();
-	}
+	
 
 }
